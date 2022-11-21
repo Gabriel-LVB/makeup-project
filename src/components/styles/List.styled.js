e77d0b21c6955f4052c1a821e584a317f15abecb
Gabriel-LVB <gabrielvieiraxc12@gmail.com>
2022-11-21 08:28:46 -0300

    fix nav list error

--- src/components/styles/List.styled.js
+++ src/components/styles/List.styled.js
@@ -55,7 +55,7 @@ export default styled.div`
       background-color: var(--bg-back);
       transition: all 0.5s ease-in-out, color 0s linear;
       font-size: 1.5rem;
-      transform: translateY(100vh);
+      transform: translateY(-100vh);
       height: 0;
       text-transform: Capitalize;
 
